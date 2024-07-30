70e703414d1cfb4f0961781970dcd431076870df
chongyan <chongyanx@163.com>
2024-07-30 14:28:30 +0800

    add gin Cross

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,1 +1,17 @@
 package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// Cors 跨域配置
+func Cors() gin.HandlerFunc {
+	config := cors.DefaultConfig()
+	config.AllowMethods = []string{"GET", "POST"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	config.AllowOrigins = []string{"http://localhost:8080"}
+	config.AllowCredentials = true
+	return cors.New(config)
+
+}
