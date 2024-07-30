b2df5bac7b08a41d65db2c32add49cb9a855bc8d
chongyan <chongyanx@163.com>
2024-07-30 14:34:02 +0800

    update database config

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,9 +8,9 @@ import (
 // Cors 跨域配置
 func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:8080"}
 	config.AllowMethods = []string{"GET", "POST"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
-	config.AllowOrigins = []string{"http://localhost:8080"}
 	config.AllowCredentials = true
 	return cors.New(config)
 
